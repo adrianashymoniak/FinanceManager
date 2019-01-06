d5f51ce1b7b62f4728371bf25e242e93e536d8f4
Adriana Shymoniak <adrianashymoniak@gmail.com>
2019-01-06 16:49:18 +0200

    Change base dir for templates.

--- finance_manager_app/settings/base_settings.py
+++ finance_manager_app/settings/base_settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'finance_manager_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
