0bfee28b6c02c2846f26e1322a508ad6fcf17b8e
Adriana Shymoniak <adrianashymoniak@gmail.com>
2019-01-07 20:20:14 +0200

    Change STATICFILES_STORAGE variable

--- finance_manager_app/settings/base_settings.py
+++ finance_manager_app/settings/base_settings.py
@@ -124,4 +124,4 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
