c20d18665bae1496dd4cf5c02fecdd61552c4ad0
Adriana Shymoniak <adrianashymoniak@gmail.com>
2019-01-07 21:18:30 +0200

    Add styles in base.css

--- finance_manager_app/settings/base_settings.py
+++ finance_manager_app/settings/base_settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'finance_manager_app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
