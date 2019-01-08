2ccd338d107aca60e1e16a7164d3bd813235744b
Adriana Shymoniak <adrianashymoniak@gmail.com>
2019-01-08 09:22:45 +0200

    Fix sign-up functionality, edit styles

--- finance_manager/views.py
+++ finance_manager/views.py
@@ -14,7 +14,7 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.changed_data.get('username')
+            username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
